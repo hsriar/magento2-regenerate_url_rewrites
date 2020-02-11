590c0501bc76c936246809e2ee46d39b471c4a67
hsriar <59007538+hsriar@users.noreply.github.com>
2020-02-11 11:46:56 +0530

    Add files via upload

---
+++ HsRiar/RegenerateUrlRewrites/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Regenerate Url rewrites
+ *
+ * @package HsRiar_RegenerateUrlRewrites
+ * @author Hs Riar <hsriar.work@gmail.com>
+ * @copyright 2020 Hs Riar
+ * @license OSL-3.0, AFL-3.0
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'HsRiar_RegenerateUrlRewrites',
+    __DIR__
+);
